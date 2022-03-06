baa4b0a987dada3ef459d2dc02b455e92548c006
EC2 Default User <ec2-user@ip-172-31-16-149.ec2.internal>
2022-03-06 14:34:36 +0000

    ex4

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,14 +1,14 @@
 class Book < ApplicationRecord
-
-  belongs_to :user
+  has_one_attached :profile_image
+  belongs_to :user, optional: true
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-
-  validates :title, presence: true
-  validates :body, presence: true, length:{maximum:200}
+  
+  validates :title,presence:true
+  validates :body,presence:true,length:{maximum:200}
 
   def favorited_by?(user)
-    favorites.exists?(user_id: user.id)
+    favorites.where(user_id: user.id).exists?
   end
 
 end
